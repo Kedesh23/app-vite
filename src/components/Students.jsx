76a25028fc8ed4f7dea11ee4c7b7997927fe223e
IKISSA PENDY Sevan Kedesh <pendysevan10@gmail.com>
2023-10-31 11:18:51 +0100

    find done

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -14,6 +14,7 @@ export default function Students() {
                 setData(data.data);
             }).catch((error) => {
                 console.log(error);
+                
             })
     }, []);
     console.log(data);
